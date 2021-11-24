317039a5a2f60a438c3c83085c99555c2f6680aa
Mahsa <mahsa.alikhany@gmail.com>
2021-11-24 14:19:41 +0330

    added setNewStudent method to StudentService

--- src/ir/maktab/service/StudentService.java
+++ src/ir/maktab/service/StudentService.java
@@ -1,7 +1,27 @@
 package ir.maktab.service;
 
+import ir.maktab.repository.StudentDao;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class StudentService {
+    private final StudentDao studentDao;
+
+    public StudentService() throws SQLException, ClassNotFoundException {
+        studentDao = new StudentDao();
+    }
+
+    public int setNewStudent(String name) throws SQLException {
+        int studentId;
+        if(studentDao.findStudentByName(name)){
+            studentId = studentDao.saveNewStudent(name);
+        }else {
+            studentId = studentDao.findStudentIdByName(name);
+        }
+        return studentId;
+    }
 }
