534c015fd113a79531630447cd5927370eeb9ea5
Mahsa <mahsa.alikhany@gmail.com>
2021-11-24 16:45:22 +0330

    added setNewRateAndComment method to StudentCourseRatingService

--- src/ir/maktab/service/StudentCourseRatingService.java
+++ src/ir/maktab/service/StudentCourseRatingService.java
@@ -1,7 +1,22 @@
 package ir.maktab.service;
 
+import ir.maktab.repository.StudentCourseRatingDao;
+
+import java.sql.SQLException;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class StudentCourseRatingService {
+    private final StudentCourseRatingDao studentCourseRatingDao;
+
+    public StudentCourseRatingService() throws SQLException, ClassNotFoundException {
+        studentCourseRatingDao = new StudentCourseRatingDao();
+    }
+
+    public void setNewRateAndComment(int courseId, int studentId, String rating, String comment) throws SQLException {
+        if(studentCourseRatingDao.findRatingsById(studentId, courseId)){
+            studentCourseRatingDao.saveNewRateAndComment(courseId, studentId, Double.valueOf(rating), comment);
+        }
+    }
 }
