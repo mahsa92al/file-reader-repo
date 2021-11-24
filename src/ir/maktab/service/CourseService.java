16f4dbfa45d8afbbbdd12b7119ca88ae2369526c
Mahsa <mahsa.alikhany@gmail.com>
2021-11-24 13:24:52 +0330

    added setNewCourse to CourseService

--- src/ir/maktab/service/CourseService.java
+++ src/ir/maktab/service/CourseService.java
@@ -1,7 +1,25 @@
 package ir.maktab.service;
 
+import ir.maktab.repository.CourseDao;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class CourseService {
+    private final CourseDao courseDao;
+
+    public CourseService() throws SQLException, ClassNotFoundException {
+        courseDao = new CourseDao();
+    }
+
+    public int setNewCourse(String courseName, String timestamp) throws SQLException {
+        int courseId = 0;
+        if(courseDao.findCourseByName(courseName)){
+            courseId = courseDao.saveNewCourse(courseName, Timestamp.valueOf(timestamp));
+        }
+        return courseId;
+    }
 }
