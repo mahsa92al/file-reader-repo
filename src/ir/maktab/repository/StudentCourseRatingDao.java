2c4b2a5c80a99dd12f0adf6e5da12e6f36037fd1
Mahsa <mahsa.alikhany@gmail.com>
2021-11-24 16:42:06 +0330

    added findRatingById method to StudentCourseRatingDao

--- src/ir/maktab/repository/StudentCourseRatingDao.java
+++ src/ir/maktab/repository/StudentCourseRatingDao.java
@@ -1,7 +1,10 @@
 package ir.maktab.repository;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import ir.maktab.model.StudentCourseRating;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -12,4 +15,16 @@ public class StudentCourseRatingDao extends BaseDao{
     public StudentCourseRatingDao() throws SQLException, ClassNotFoundException {
         connection = BaseDao.getConnection();
     }
+
+    public boolean findRatingsById(int studentId, int courseId) throws SQLException {
+        boolean isEmpty = true;
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(String.format("select course_id from course" +
+                " where student_id = %d and course_id = %d", studentId, courseId));
+        while (resultSet.next()){
+            isEmpty = false;
+        }
+        return isEmpty;
+    }
+
 }
