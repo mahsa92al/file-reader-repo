1a7d5626a3d139d05337be1c2865d20b9718e01c
Mahsa <mahsa.alikhany@gmail.com>
2021-11-24 11:56:34 +0330

    added findCourseByName method to CourseDao

--- src/ir/maktab/repository/CourseDao.java
+++ src/ir/maktab/repository/CourseDao.java
@@ -1,7 +1,9 @@
 package ir.maktab.repository;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -11,5 +13,15 @@ public class CourseDao extends BaseDao{
 
     public CourseDao() throws SQLException, ClassNotFoundException {
         connection = BaseDao.getConnection();
+    }
+
+    public boolean findCourseByName(String courseName) throws SQLException {
+        boolean isEmpty = true;
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(String.format("select * from course where name = '%s'", courseName));
+        while (resultSet.next()){
+            isEmpty = false;
+        }
+        return isEmpty;
     }
 }
