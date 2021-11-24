daf6a3a8dec113f3f5a25b748554df205d4fa022
Mahsa <mahsa.alikhany@gmail.com>
2021-11-24 16:47:53 +0330

    added readFileAndSaveToDBTable method to Main

--- src/ir/maktab/Main.java
+++ src/ir/maktab/Main.java
@@ -15,5 +15,34 @@ import java.util.Scanner;
  * @author Mahsa Alikhani m-58
  */
 public class Main {
+    private static StudentService studentService;
+    private static CourseService courseService;
+    private static StudentCourseRatingService studentCourseRatingService;
+    public static void main(String[] args) throws Exception {
+        studentService = new StudentService();
+        courseService = new CourseService();
+        studentCourseRatingService = new StudentCourseRatingService();
+        readFileAndSaveToDBTable();
+    }
 
+    private static void readFileAndSaveToDBTable() throws Exception {
+        String pathname = "D:\\BootcampSharif\\problemSets\\pset13-designPattern-IO\\IO\\studentCourseRating.txt";
+        Path dataFileUrl = Paths.get(pathname);
+        if(Files.notExists(dataFileUrl)){
+            throw new Exception("File is not exist!");
+        }
+
+        try(Scanner scanner = new Scanner(new File(pathname))){
+            String headerLine = scanner.nextLine();
+            while (scanner.hasNext()){
+                String line = scanner.nextLine();
+                String[] splitLine = line.split(",");
+                int courseId = courseService.setNewCourse(splitLine[0], splitLine[2]);
+                int studentId = studentService.setNewStudent(splitLine[1]);
+                studentCourseRatingService.setNewRateAndComment(courseId, studentId, splitLine[3], splitLine[4]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
