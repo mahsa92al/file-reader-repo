d1b02c4da2e5f9edfcff2f38196eba48dbb92c9e
Mahsa <mahsa.alikhany@gmail.com>
2021-11-24 14:13:07 +0330

    added findStudentByName method to StudentDao

--- src/ir/maktab/repository/StudentDao.java
+++ src/ir/maktab/repository/StudentDao.java
@@ -1,7 +1,9 @@
 package ir.maktab.repository;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -11,5 +13,15 @@ public class StudentDao extends BaseDao{
 
     public StudentDao() throws SQLException, ClassNotFoundException {
         this.connection = BaseDao.getConnection();
+    }
+
+    public boolean findStudentByName(String name) throws SQLException {
+        boolean isEmpty = true;
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(String.format("select * from student where name = '%s'", name));
+        while (resultSet.next()){
+            isEmpty = false;
+        }
+        return isEmpty;
     }
 }
