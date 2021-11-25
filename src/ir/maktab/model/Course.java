ada848cd3c9d1aff834c8c035aa468b56c20b539
Mahsa <mahsa.alikhany@gmail.com>
2021-11-25 22:48:25 +0330

    added id property to Course

--- src/ir/maktab/model/Course.java
+++ src/ir/maktab/model/Course.java
@@ -6,8 +6,17 @@ import java.sql.Timestamp;
  * @author Mahsa Alikhani m-58
  */
 public class Course {
+    private int id;
     private String name;
     private Timestamp timestamp;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
